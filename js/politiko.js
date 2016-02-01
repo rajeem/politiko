e937d075abe05850ce1a8c5f0ad76af4b48fdd9a
Pepe Bawagan <syk0saje@gmail.com>
2016-02-01 22:34:23 +0800

    uses csvs for data

--- js/politiko.js
+++ js/politiko.js
@@ -1,65 +1,69 @@
 var app = angular.module('politiko', ['ui', 'ui.bootstrap']);
 
-app.controller('Politiko', function($scope){
+app.controller('Politiko', function ($scope){
 
-	////////////////// PARSE INPUT ///////////////////
+	$scope.pending = 0;
+
+	['cands', 'issues', 'stances', 'sources'].forEach(function (table){
+		$scope[table] = [];
+		$scope.pending++;
+		Papa.parse('data/' + table + '.csv', {
+			download: true,
+			header: true,
+			complete: function (r){
+				$scope[table] = r.data;
+				if (!--$scope.pending){ $scope.postProcessing(); }
+			}
+		});
+	});
 
 	$scope.positions = [
 		{name: "President", id: "p"},
-		{name: "Vice President", id: "vp"}
+		{name: "Vice President", id: "vp"},
+		// {name: "Senator", id: "s"}
 	];
-	$scope.cands = [];
-	$scope.issues = [];
 
-	for (var pos in data){
-		for (var cand in data[pos]){
-			for (var issue in data[pos][cand]){
-				$scope.issues.push({name: issue});
-			}
-			break;
-		}
-		break;
+	$scope.postProcessing = function (){
+		$scope.offset = $scope.issues.length * 2 + 10; // used for offsetting negative values when sorting candidates
+		$scope.curIssue = $scope.issues[0];
 	}
 
-	for (var pos in data){
-		for (var cand in data[pos]){
-			$scope.cands.push({
-				name: cand,
-				stances: data[pos][cand],
-				breakdown: [],
-				score: 0,
-				pos: pos
-			})
-		}
-	}
-
-	$scope.offset = $scope.issues.length * 2 + 10; // used for offsetting negative values when sorting candidates
-
-	$scope.stances = ['for', 'against', 'noStand', 'NA'];
 	$scope.isCollapsed = true;
-	$scope.curIssue = $scope.issues[0];
+
+	$scope.stanceMap = {
+		for: 1,
+		noStand: 0,
+		NA: 0,
+		against: -1
+	}
 
 	/////////////////// END OF PARSING //////////////////////////
 
 	$scope.view = 'quiz';
 	$scope.includeDeductions = {value: true};
 
-	$scope.setView = function(view) {
+	$scope.setView = function (view) {
 		$scope.view = view;
 	}
 
-	$scope.setCurIssue = function(issue){
+	$scope.setCurIssue = function (issue){
 		$scope.curIssue = issue;
 	}
 
-	$scope.candOrder = function(cand){
+	$scope.candOrder = function (cand){
 		return [$scope.offset - cand.score, cand.name];
 	}
 
-	$scope.update = function(){
+	$scope.source = function(cand, issue){
+		return $scope.sources.find(function (stance){
+			return stance.name == cand.name;
+		})[issue.name];
+	}
 
-		$('div#question').fadeOut(200, function(){
-			$scope.$apply(function(){
+	$scope.update = function (){
+
+		$('div#question').fadeOut(200, function (){
+			$scope.$apply(function (){
 
 				for(var i in $scope.cands){
 
@@ -72,18 +76,13 @@ app.controller('Politiko', function($scope){
 						var candStance = $scope.stanceMap[$scope.getStance(cand, issue)];
 						var weight = myStance * candStance;
 
-						var pushCondition = null;
-						if($scope.includeDeductions.value){
-							pushCondition = !!weight;
-						} else {
-							pushCondition = (weight > 0);
-						}
-
-						if(pushCondition){
+						if(!!weight){
 							cand.breakdown.push({
 								name: issue.name,
 								weight: weight,
-								src: cand.stances[issue.name][1]
+								src: $scope.sources.find(function (src){
+									return src.name == cand.name;
+								})[issue.name]
 							});
 							score += weight;
 						}
@@ -103,27 +102,19 @@ app.controller('Politiko', function($scope){
 
 	}
 
-	$scope.stanceMap = {
-		for: 1,
-		noStand: 0,
-		against: -1
-	}
-
-	$scope.candId = function(cand){
-		return $scope.cands.indexOf(cand);
-	}
-
 	$scope.getCands = function (pos){
 		return $scope.cands.filter(function (c){
 			return c.pos === pos.id;
 		});
 	}
 
-	$scope.getStance = function(cand, issue){
-		return cand.stances[issue.name][0];
+	$scope.getStance = function (cand, issue){
+		return $scope.stances.find(function (s){
+			return s.name == cand.name;
+		})[issue.name];
 	}
 
-	$scope.getIcon = function(weight){
+	$scope.getIcon = function (weight){
 		var result;
 		switch(weight) {
 			case "-2":
